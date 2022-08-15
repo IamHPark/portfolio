24412e3b7f106296fa28feebfee07b75f75cb443
Heesoo <heesoo.p1023@gmail.com>
2022-08-15 14:26:54 -0700

    main page structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import './Main.css';
 
 export default function Main() {
     return (
-        <div className="main-container">
-            <h1>Hello, I'm Heesoo</h1>
-            <h1>A Full-stack Web Developer</h1>
-
+        <>
+            <div className="main-container">
+                <div className="main-content">
+                    <h1>Hello, I'm Heesoo</h1>
+                    <h1>A Full-stack Web Developer</h1>
+                </div>
+                <img src="/girl.jpg" alt="" width={"800px"}/>
+            </div>
             <button className="main-btn">start</button>
-        </div>
+        </>
     )
 }
